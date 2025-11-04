36b67b7804994e1f2b4a4f196c021d52f5210c0b
Bazie521 <MTHBAZ001@myuct.ac.za>
2025-11-04 21:36:46 +0200

    First commit after environment setup

---
+++ numberrangesummarizer/NumberRangeMain.java
@@ -0,0 +1,19 @@
+package numberrangesummarizer;
+
+import java.util.Collection;
+
+public class NumberRangeMain implements NumberRangeSummarizer {
+  
+    public static void main(String[] args){
+        
+    }
+
+    public Collection<Integer> collect(String input){
+        return null;
+    }
+
+    public String summarizeCollection(Collection<Integer> input){
+        return "";
+    }
+
+}
