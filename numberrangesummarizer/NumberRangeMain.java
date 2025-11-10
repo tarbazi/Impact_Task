08aa164ede1d3717fdbbff7b9f8d2570e58fd940
Bazie521 <MTHBAZ001@myuct.ac.za>
2025-11-10 19:52:48 +0200

    Implemented collect function. Setup summarizeCollection.

--- numberrangesummarizer/NumberRangeMain.java
+++ numberrangesummarizer/NumberRangeMain.java
@@ -1,19 +1,44 @@
 package numberrangesummarizer;
 
 import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
 
 public class NumberRangeMain implements NumberRangeSummarizer {
-  
-    public static void main(String[] args){
-        
+    
+    public NumberRangeMain(){
+    
     }
-
+    
     public Collection<Integer> collect(String input){
-        return null;
+        
+        String[] myTempArr = input.split(",");
+        Collection<Integer> myTempCollection = new ArrayList<>();
+
+        for (String tempStr: myTempArr){
+            myTempCollection.add(Integer.parseInt(tempStr));
+        }
+        
+        return myTempCollection;
+
     }
 
     public String summarizeCollection(Collection<Integer> input){
-        return "";
+        
+        String finalStr = "";
+
+        List<Integer> inputList = new ArrayList<>(input); 
+
+        for (int i = 1; i < input.size() - 1; i++ ){
+
+            /*if (inputList.get(i - 1) != inputList.get(i) - 1){
+                finalStr += String.valueOf(inputList.get(i));
+            }*/
+
+        }
+        
+        return finalStr;
+
     }
 
 }
