592e94747883833c3f833767d9e116bbd14af976
Bazie521 <MTHBAZ001@myuct.ac.za>
2025-11-10 22:26:34 +0200

    1 of 5 test cases fails on summarizeCollection.

--- test/NumberRangeMainUnitTests.java
+++ test/NumberRangeMainUnitTests.java
@@ -3,7 +3,6 @@ package test;
 import java.util.List;
 import java.util.Arrays;
 import java.util.ArrayList;
-import java.util.Collection;
 
 import numberrangesummarizer.NumberRangeMain;
 
@@ -13,6 +12,8 @@ public class NumberRangeMainUnitTests {
         NumberRangeMain myObj = new NumberRangeMain();
 
         runCollectTests(myObj);
+        runSummarizeCollection(myObj);
+
     }
 
     public static void runCollectTests(NumberRangeMain myObj){
@@ -44,16 +45,50 @@ public class NumberRangeMainUnitTests {
                 passed++;
             }
             else{
-                System.out.println("Test case failed: " + myObj.collect(inputs.get(i)) + " matches expected output " + outputs.get(i));
+                System.out.println("Test case failed: " + myObj.collect(inputs.get(i)) + " does not match expected output " + outputs.get(i));
             }
             count++;
         }
 
-        System.out.println(100*(passed/count) + "% test cases passed.");
+        System.out.println((100*passed/count) + "% test cases passed.");
     }
 
-    public void runSummarizeCollection(){
+    public static void runSummarizeCollection(NumberRangeMain myObj){
+
+        int count = 0;
+        int passed = 0;
         
+        List<List<Integer>> inputs = new ArrayList<List<Integer>>(Arrays.asList(
+            new ArrayList<Integer>(Arrays.asList(1,3,6,7,8,12,13,14,15,21,22,23,24,31)),
+            new ArrayList<Integer>(Arrays.asList(2,3,4,7,9,12,18,20,29)),
+            new ArrayList<Integer>(Arrays.asList(10,20,30,40,50,60,70,80,90,95,100)),
+            new ArrayList<Integer>(Arrays.asList(2001)),
+            new ArrayList<Integer>(Arrays.asList(1,3,4,5,7,8,9))
+            )
+        );
+
+        List<String> outputs = new ArrayList<String>(Arrays.asList(
+            "1, 3, 6-8, 12-15, 21-24, 31",
+            "2-4, 7, 9, 12, 18, 20, 29",
+            "10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 100",
+            "2001",
+            "1, 3-5, 7-9"
+            )
+        );
+
+        for (int i = 0; i < inputs.size(); i++){
+            if (myObj.summarizeCollection(inputs.get(i)).equals(outputs.get(i))){
+                System.out.println("Test case passed: " + myObj.summarizeCollection(inputs.get(i)) + " matches expected output " + outputs.get(i));
+                passed++;
+            }
+            else{
+                System.out.println("Test case failed: " + myObj.summarizeCollection(inputs.get(i)) + " does not match with expected output " + outputs.get(i));
+            }
+            count++;
+        }
+
+        System.out.println((100*passed/count) + "% test cases passed.");
+
     }
     
 }
