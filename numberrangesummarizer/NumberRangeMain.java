410b7e0e7194841df61f275637ea8505b0fced07
Bazie521 <MTHBAZ001@myuct.ac.za>
2025-11-10 20:49:06 +0200

    Collect method test passes. SummarizeCollection method omits last element of array, to fix bug.

--- numberrangesummarizer/NumberRangeMain.java
+++ numberrangesummarizer/NumberRangeMain.java
@@ -1,8 +1,9 @@
 package numberrangesummarizer;
 
-import java.util.Collection;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class NumberRangeMain implements NumberRangeSummarizer {
     
@@ -13,11 +14,13 @@ public class NumberRangeMain implements NumberRangeSummarizer {
     public Collection<Integer> collect(String input){
         
         String[] myTempArr = input.split(",");
-        Collection<Integer> myTempCollection = new ArrayList<>();
+        List<Integer> myTempCollection = new ArrayList<>();
 
         for (String tempStr: myTempArr){
             myTempCollection.add(Integer.parseInt(tempStr));
         }
+
+        Collections.sort(myTempCollection);
         
         return myTempCollection;
 
@@ -29,16 +32,31 @@ public class NumberRangeMain implements NumberRangeSummarizer {
 
         List<Integer> inputList = new ArrayList<>(input); 
 
-        for (int i = 1; i < input.size() - 1; i++ ){
+        for (int i = 0; i < input.size() - 1; i++ ){
 
-            /*if (inputList.get(i - 1) != inputList.get(i) - 1){
+            if (i != 0){
+                finalStr += ", ";
+            }
+
+            if (inputList.get(i) + 1 == inputList.get(i+1)){
+                for (int j = i + 2; j < input.size(); j++){
+
+                    if (inputList.get(j-1) + 1 != inputList.get(j)){
+                        finalStr += String.valueOf(inputList.get(i)) +"-"+ String.valueOf(inputList.get(j-1));
+                        i = j;
+                        break;
+                    }
+
+                }
+            }
+
+            else{
                 finalStr += String.valueOf(inputList.get(i));
-            }*/
-
+            }
+            
         }
-        
-        return finalStr;
 
+        return finalStr;  
     }
 
 }
---
+++ test/NumberRangeMainUnitTests.java
@@ -0,0 +1,18 @@
+package test;
+
+import java.util.Collection;
+
+import numberrangesummarizer.NumberRangeMain;
+
+public class NumberRangeMainUnitTests {
+    
+    public static void main(String[] args){
+        NumberRangeMain myObj = new NumberRangeMain();
+
+        Collection<Integer> myVals = myObj.collect("1,3,6,7,8,12,13,14,15,21,22,23,24,31,19");
+        System.out.println(myVals);
+
+        System.out.println(myObj.summarizeCollection(myVals));
+    }
+    
+}
